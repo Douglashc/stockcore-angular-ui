11f0354f161a3f3f9355e88fddaf4c23910571e4
Douglashc <douglash.dcz@gmail.com>
2026-05-31 20:49:42 -0400

    feat(modules): Add auth and dashboard modules

---
+++ src/app/auth/auth-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: AuthLayoutComponent,
+    children: [
+
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AuthRoutingModule { }
